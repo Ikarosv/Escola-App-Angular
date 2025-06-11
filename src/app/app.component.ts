08cc3cf745906d237a3961d70b07e42efaa69c72
Ikaro Vieira <ikarovieiraa@gmail.com>
2025-06-11 11:13:48 -0300

    Exibição de disciplinas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaDeDisciplinasComponent } from './lista-de-disciplinas/lista-de-disciplinas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, NgStyle, ListaDeDisciplinasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'escola-app';
+  isSpecial = true
+  currentStyles: Record<string, string> = {}
+  ngOnInit(): void {
+    this.setCurrentStyles()
+  }
+  setCurrentStyles() {
+    this.currentStyles = {
+      'font-size': '24px'
+    }
+  }
 }
--- src/app/lista-de-disciplinas/lista-de-disciplinas.component.ts
+++ src/app/lista-de-disciplinas/lista-de-disciplinas.component.ts
@@ -1,11 +1,29 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Disciplina } from './disciplina.model';
 
 @Component({
   selector: 'app-lista-de-disciplinas',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './lista-de-disciplinas.component.html',
   styleUrl: './lista-de-disciplinas.component.css'
 })
-export class ListaDeDisciplinasComponent {
 
+export class ListaDeDisciplinasComponent {
+  selecionado: Disciplina | null = null;
+  disciplinas: Disciplina[] = [
+    new Disciplina('Língua Portuguesa', 'Estudo da língua portuguesa, incluindo gramática, interpretação de textos, ortografia e produção textual.'),
+    new Disciplina('Arte', 'Exploração de diversas formas de expressão artística, como pintura, música, teatro e história da arte.'),
+    new Disciplina('Educação Física', 'Prática de atividades físicas e esportes visando o desenvolvimento motor, social e da saúde.'),
+    new Disciplina('Matemática', 'Estudo dos números, operações, geometria, álgebra, estatística e raciocínio lógico.'),
+    new Disciplina('História', 'Análise de eventos históricos, sociedades antigas e modernas, e a construção da cidadania.'),
+    new Disciplina('Geografia', 'Estudo do espaço geográfico, clima, relevo, populações, e suas interações com o meio ambiente.'),
+    new Disciplina('Ciências', 'Compreensão dos fenômenos naturais através da biologia, química, física e ciências da Terra.'),
+    new Disciplina('Redação', 'Desenvolvimento da escrita formal com foco em coesão, coerência e argumentação.'),
+    new Disciplina('Língua Estrangeira Moderna - Inglês', 'Aprendizado da língua inglesa com foco em leitura, escrita, compreensão e conversação.'),
+    new Disciplina('Ensino Religioso', 'Estudo das diferentes religiões, valores éticos e morais, promovendo respeito e tolerância.')
+  ];
+  selecionar(disciplina: Disciplina) {
+    this.selecionado = disciplina
+  }
 }
